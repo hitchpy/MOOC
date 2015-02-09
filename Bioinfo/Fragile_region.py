2406991d313af0d1025518c86d7359fcf05baa10
hitchpy <hitchpy@gmail.com>
2015-02-08 18:57:46 -0800

    count breakpoint

--- Bioinfo/Fragile_region.py
+++ Bioinfo/Fragile_region.py
@@ -22,6 +22,22 @@ def greedysorting(P):
 
 	return(approxReversalDistance)
 
+def countBreakpoint(P):
+	'''
+	Given a list of numbers with signs, find the number of break point
+	define as P[k+1] - P[k] != 1, for k in 0...len(P).
+	Add in 0 and len(P)+1 to the original P [3, 1, 2] -> [0, 3, 1, 2, 4]
+	Count = 3
+	'''
+	res = 0
+	P.append(len(P)+1); P.insert(0, 0)
+	for k in range(len(P)-1):
+		if P[k+1] - P[k] != 1:
+			res += 1
+	return(res)
+
+
+
 def main():
 	fin = open(sys.argv[1], 'r')
 	#fin.readline()
@@ -30,7 +46,8 @@ def main():
 	dat = dat[1:(len(dat)-1)]
 	dat = dat.split()
 	P = [int(x) for x in dat]
-	greedysorting(P)
+	#greedysorting(P)
+	print(str(countBreakpoint(P)))
 	print('done\n')
 
 
